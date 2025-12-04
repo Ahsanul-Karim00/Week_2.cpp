78a848703d8c4ab533fe6efa9ac585e73a1aa24c
Ahsanul-karim00 <ahsanulkarim879708@gmail.com>
2025-12-04 22:32:11 +0600

    name

---
+++ Squid_Game_Piggy_Bank.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int sub = n - k;
+    int mul = sub * 10000;
+    cout << mul << endl;
+    return 0;
+}
